c854b765f48874ff2f6c3edadff9621b582edee4
IrinAnoVo <irinapavelviktor@gmail.com>
2024-12-27 22:12:47 +0100

    Add files via upload
    T3

--- script.js
+++ script.js
@@ -1,115 +1,97 @@
-console.log('hello')
-// 11. Напишите метод, который принимает массив чисел и возвращает их сумму.
-function sumArray(numbers) {
-   let sum = 0;
-   for (let i = 0; i < numbers.length; i++) {
-       sum = sum + numbers[i];
-   }
-   console.log(sum)
+// 1. Напишите метод, который принимает массив целых чисел и возвращает его длину.
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+function getArrayLength(array) {
+    for (let i = 0; i <= array.length; i++) {
+        if (i === array.length - 1) {
+            return i + 1;
+        } else {
+            continue;
+        }
+    }    
 }
+    console.log(getArrayLength(array));
 
-// 12. Напишите метод, который принимает два числа и возводит первое в степень второго.
-function power(base, exponent) {
-   let result = 1;
-   for (let i = 0; i < exponent; i++) {
-       result = result * base;
-   }
-   console.log(result)
+// 2. Напишите метод, который принимает массив целых чисел и выводит его элементы в консоль.   
+function printArray(array) {
+    for (let i = 0; i < array.length; i++) {
+    return array;
+    } 
 }
+console.log(array);
 
-// 13. Напишите метод, который проверяет, является ли число простым.
-function isPrime(number) {
-   for (let i = 2; i < number; i++) {
-       if (number % i == 0) {
-           return false;
-       }
-   }
-return true;
-}
-
-// 14. Напишите метод, который принимает два числа и возвращает их наибольший общий делитель (НОД).
-function gcd(num1, num2) {
-    while (num2 !== 0) {
-        let temp = num2;
-        num2 = num1 % num2;
-        num1 = temp;
+// 3. Напишите метод, который принимает массив целых чисел и возвращает сумму его элементов.
+function sumArrayElements(array) {
+    let sum = 0; 
+    for (let i = 0; i < array.length; i++) {
+        sum = sum + array[i];         
     }
-    return num1;
+    return sum;
 }
+console.log(sumArrayElements(array));
 
-
-// 15. Напишите метод, который принимает массив чисел и возвращает его максимальный элемент.
-function maxInArray(numbers) {
-   let max = numbers[0];
-   for (let i = 1; i < numbers.length; i++) {
-       if (numbers[i] > max) {
-           max = numbers[i];
-       }
-   }
-   return max;
-}
-
-// 16. Напишите метод, который принимает массив чисел и возвращает его минимальный элемент.
-function minInArray(numbers) {
-   let min = numbers[0];
-   for (let i = 1; i < numbers.length; i++) {
-       if (numbers[i] < min) {
-           min = numbers[i];
-       }
-   }
-   return min;
-}
-
-// 17. Напишите метод, который проверяет, является ли число палиндромом
-// (одинаково читается слева направо и справа налево).
-function isPalindrome(number) {
-   let palindrome = number.toString();
-   for (let i = 0; i < palindrome.length() / 2; i++)
-       if (palindrome.charAt(i)!= palindrome.charAt(palindrome.length() - i - 1)) {
-           return false;
-       }
-   return true;
-   }
-
-// 18. Напишите метод, который возвращает n-ое число Фибоначчи.
-function fibonacci(n) {
-   if (n < 0) {
-       throw new Errer('n must be non-negative');
-   }
-   if (n == 0) {
-       return 0;
-   }
-   if (n == 1) {
-       return 1;
-   }
-   let fib1 = 0; // Начальные два
-   let fib2 = 1; // числа Фибоначчи
-   for (let i = 2; i <= n; i++) {
-    let temp = fib1 + fib2;
-       fib1 = fib2;
-       fib2 = temp;
-   }
-   return fib2; // Возвращаем n-е число Фибоначчи
-}
-
-// 19. Напишите метод, который принимает массив чисел и сортирует его по возрастанию.
-function sortArray(numbers) {
-   return numbers.sort((num1, num2) => num1 - num2);
-}
-
-// 20. Напишите метод, который принимает два числа и возвращает их наименьшее общее кратное (НОК).
-function gcd(num1, num2) {
-    while (num2 !== 0) { //алгоритм Евклида
-        let temp = num2;
-        num2 = num1 % num2;
-        num1 = temp;
+// 4. Напишите метод, который принимает массив целых чисел и возвращает максимальный элемент.
+let iarray = [1, 2, 3, 6, 7, 8, 12, 10];
+function findMaxElement(iarray) {
+   let max = iarray[0];
+    for (let i = 1; i < iarray.length; i++) {
+        if (iarray[i] > max) {
+            max = iarray[i];
+        }
     }
-    return num1;
+    return max;
 }
+console.log(findMaxElement(iarray));
 
-function lcm(num1, num2) {
-    if (num1 === 0 || num2 === 0) {
-        return 0;
-    return Math.abs(num1 * num2) / gcd(num1, num2); //формула для НОК
-    }
-}
+//5. Напишите метод, который принимает массив целых чисел и возвращает минимальный элемент.
+    //function findMinElement(array) {
+        // TODO: реализовать метод
+        //return 0;
+ 
+// 6. Напишите метод, который принимает массив целых чисел и возвращает
+       // * новый массив, содержащий только чётные элементы. 
+        //public static int[] filterEvenNumbers(int[] array) {
+        // TODO: реализовать метод
+        //return new int[0];
+    //}
+
+    /**
+     * 7. Напишите метод, который принимает массив целых чисел и
+     * возвращает массив в обратном порядке.
+     */
+    //public static int[] reverseArray(int[] array) {
+        // TODO: реализовать метод
+        //return new int[0];
+    //}
+
+    /**
+     * 8. Напишите метод, который принимает массив целых чисел и возвращает true,
+     * если массив отсортирован по возрастанию.
+     */
+    //public static boolean isArraySorted(int[] array) {
+        // TODO: реализовать метод
+        //return false;
+    //}
+
+    /**
+     * 9. Напишите метод, который принимает массив целых чисел и возвращает
+     * среднее арифметическое его элементов.
+     */
+    //public static double calculateAverage(int[] array) {
+        // TODO: реализовать метод
+        //return 0.0;
+    //}
+
+    /**
+     * 10. Напишите метод, который принимает массив целых чисел и
+     * возвращает количество чётных чисел в массиве.
+     */
+    //public static int countEvenNumbers(int[] array) {
+        // TODO: реализовать метод
+        //return 0;
+    //}
+
+    //public static void main(String[] args) {
+        // Пример вызовов методов (после их реализации)
+        // int[] exampleArray = {1, 2, 3, 4, 5};
+        // System.out.println(getArrayLength(exampleArray));
+    
